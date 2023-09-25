8a33770af92b3f1834c8f73ba20983117144e634
SannDcode <108715412+SannDcode@users.noreply.github.com>
2023-09-25 11:52:44 +0700

    Add files via upload

---
+++ kella.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <conio.h>
+#include <string>
+using namespace std;
+
+// ALLOO KELLA :V
+int main()
+{
+    // MULAI CODE
+
+    system("cls"); // Bersihin layar
+
+    string ungu = "\x1b[35m"; // ungu
+    string batas = " -------------------\n";
+    string pinggiranKri = " <  ";
+    string pinggiranKna = "   >";
+    string Alo = " Alo Kella_<";
+    string enter = "\n";
+    string risetClr = "\x1b[0m";
+
+    // MENAMPILKAN OUTPUT DARI CODENYA
+    cout << ungu << enter << batas << pinggiranKri << Alo << pinggiranKna << enter << batas << risetClr;
+
+    return 0;
+}
